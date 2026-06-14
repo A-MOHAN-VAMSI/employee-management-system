5f170a4e6ae6731add19b2312db8fc58078db6ba
A-MOHAN-VAMSI <2400032078@kluniversity.in>
2026-06-14 20:55:02 +0530

    initiate all

---
+++ employee-management-system/backend/routes/employeeRoutes.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const router = express.Router();
+
+const Employee = require("../models/Employee");
+
+router.post("/", async(req,res)=>{
+    try{
+        const employee = new Employee(req.body);
+        await employee.save();
+        res.status(201).json(employee);
+    }catch(error){
+        res.status(500).json({message:error.message});
+    }
+});
+
+router.get("/", async(req,res)=>{
+    try{
+        const employees = await Employee.find();
+        res.json(employees);
+    }catch(error){
+        res.status(500).json({message:error.message});
+    }
+});
+
+router.put("/:id", async(req,res)=>{
+    try{
+        const employee = await Employee.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new:true}
+        );
+        res.json(employee);
+    }catch(error){
+        res.status(500).json({message:error.message});
+    }
+});
+
+router.delete("/:id", async(req,res)=>{
+    try{
+        await Employee.findByIdAndDelete(req.params.id);
+        res.json({message:"Employee Deleted"});
+    }catch(error){
+        res.status(500).json({message:error.message});
+    }
+});
+
+module.exports = router;
---
+++ employee-management-system/frontend/js/script.js
@@ -0,0 +1,195 @@
+const API_URL = "http://localhost:5000/api/employees";
+
+const form = document.getElementById("employeeForm");
+const employeeTable = document.getElementById("employeeTable");
+let editingId = null;
+
+async function fetchEmployees(){
+
+    const res = await fetch(API_URL);
+
+    const employees = await res.json();
+
+    document.getElementById("totalEmployees")
+        .textContent = employees.length;
+
+    employeeTable.innerHTML = "";
+
+    if(employees.length === 0){
+
+        employeeTable.innerHTML = `
+            <tr>
+                <td colspan="6">
+                    No Employees Found
+                </td>
+            </tr>
+        `;
+
+        return;
+    }
+
+    employees.forEach(emp => {
+
+        employeeTable.innerHTML += `
+            <tr>
+                <td>${emp.name}</td>
+                <td>${emp.department}</td>
+                <td>${emp.role}</td>
+                <td>${emp.salary}</td>
+                <td>${new Date(emp.joinDate).toLocaleDateString()}</td>
+                <td>
+                    <button
+                        class="edit-btn"
+                        onclick="editEmployee('${emp._id}')">
+                        Edit
+                    </button>
+
+                    <button
+                        class="delete-btn"
+                        onclick="deleteEmployee('${emp._id}')">
+                        Delete
+                    </button>
+                </td>
+            </tr>
+        `;
+    });
+
+}
+async function editEmployee(id){
+
+    const res = await fetch(API_URL);
+
+    const employees = await res.json();
+
+    const employee = employees.find(emp => emp._id === id);
+
+    document.getElementById("name").value = employee.name;
+    document.getElementById("department").value = employee.department;
+    document.getElementById("role").value = employee.role;
+    document.getElementById("salary").value = employee.salary;
+
+    document.getElementById("joinDate").value =
+        employee.joinDate.split("T")[0];
+
+    editingId = id;
+}
+
+form.addEventListener("submit", async(e)=>{
+
+    e.preventDefault();
+
+    const employee = {
+
+        name: document.getElementById("name").value,
+        department: document.getElementById("department").value,
+        role: document.getElementById("role").value,
+        salary: document.getElementById("salary").value,
+        joinDate: document.getElementById("joinDate").value
+    };
+
+    if(editingId){
+
+        await fetch(`${API_URL}/${editingId}`,{
+            method:"PUT",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify(employee)
+        });
+
+        editingId = null;
+
+    }else{
+
+        await fetch(API_URL,{
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify(employee)
+        });
+
+    }
+
+    form.reset();
+
+    fetchEmployees();
+
+});
+
+async function deleteEmployee(id){
+
+    const confirmDelete =
+        confirm("Are you sure you want to delete this employee?");
+
+    if(!confirmDelete) return;
+
+    await fetch(`${API_URL}/${id}`,{
+        method:"DELETE"
+    });
+
+    fetchEmployees();
+}
+
+fetchEmployees();
+const searchInput = document.getElementById("search");
+
+searchInput.addEventListener("keyup", () => {
+
+    const searchValue = searchInput.value.toLowerCase();
+
+    const rows = document.querySelectorAll("#employeeTable tr");
+
+    rows.forEach(row => {
+
+        const text = row.textContent.toLowerCase();
+
+        row.style.display =
+            text.includes(searchValue) ? "" : "none";
+
+    });
+
+});
+let sortAsc = true;
+
+async function sortSalary(){
+
+    const res = await fetch(API_URL);
+
+    let employees = await res.json();
+
+    employees.sort((a,b)=>{
+
+        return sortAsc
+            ? a.salary - b.salary
+            : b.salary - a.salary;
+
+    });
+
+    sortAsc = !sortAsc;
+
+    employeeTable.innerHTML = "";
+
+    employees.forEach(emp => {
+
+        employeeTable.innerHTML += `
+        <tr>
+            <td>${emp.name}</td>
+            <td>${emp.department}</td>
+            <td>${emp.role}</td>
+            <td>${emp.salary}</td>
+            <td>${new Date(emp.joinDate).toLocaleDateString()}</td>
+            <td>
+                <button class="edit-btn"
+                onclick="editEmployee('${emp._id}')">
+                Edit
+                </button>
+
+                <button class="delete-btn"
+                onclick="deleteEmployee('${emp._id}')">
+                Delete
+                </button>
+            </td>
+        </tr>`;
+    });
+}
